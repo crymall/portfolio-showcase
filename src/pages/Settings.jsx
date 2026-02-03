cc70ead50fae79489d4ca5220bcaa9e3eef532fa
crymall <reed.gaines@gmail.com>
2026-02-03 14:09:16 -0500

    expand settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Tabs, TextInput, Stack, Title } from "@mantine/core";
 import useAuth from "../context/auth/useAuth";
 import useData from "../context/data/useData";
 import UserList from "../components/UserList";
@@ -15,9 +16,28 @@ const Settings = () => {
   }, [fetchUsers, user]);
 
   return (
-    <Can perform="read:users">
-      <UserList />
-    </Can>
+    <Tabs defaultValue="profile">
+      <Tabs.List>
+        <Tabs.Tab value="profile">Profile</Tabs.Tab>
+        <Can perform="read:users">
+          <Tabs.Tab value="admin">Admin Panel</Tabs.Tab>
+        </Can>
+      </Tabs.List>
+
+      <Tabs.Panel value="profile">
+        <Title order={4} mb="md">User Information</Title>
+        <Stack gap="md" maw={400}>
+          <TextInput label="Username" value={user.username} readOnly />
+          <TextInput label="Email" value={user.email || ''} readOnly />
+        </Stack>
+      </Tabs.Panel>
+
+      <Tabs.Panel value="admin">
+        <Can perform="read:users">
+          <UserList />
+        </Can>
+      </Tabs.Panel>
+    </Tabs>
   );
 };
 
